ff4beca553384ec3876f58e299242a396f1fa81e
Zeqiui Adams <107128537+QuizeCapital@users.noreply.github.com>
2022-10-14 07:15:10 +0100

    update 'Analysis Code.py'

---
+++ .history/Codes/Analysis Code Updated_20221014071501.py
@@ -0,0 +1,76 @@
+import sys 
+path = "/Users/adamszequi/Desktop/Clones/UniversalModules"
+sys.path.append(path)
+from ExternalModules import modulesSmartFactor
+import pandas as pd
+import numpy as np
+
+class ROIC():
+    
+    '''
+    This functions sets the global variables that will be used
+    for our later analysis
+    '''
+    def __init__(self, annualHistoricalPrices, annualROIC):
+        self.annualHistoricalPrices = annualHistoricalPrices
+        self.annualROIC = annualROIC
+    
+    '''
+    This function calls the openJson function from the UniversalModules
+    class. It takes the return on invested capital data and returns a list
+    of dataframe grouped by year.
+    '''
+    def splitDfYears(self):  
+        
+        rawData = self.annualROIC
+        openData = modulesSmartFactor().openJson(self.annualROIC)
+        #flatteingin our dictionary so we can easily
+        #create a dataframe of our data
+        flattenedData = [
+        (key, keyJunior, valueJunior) 
+         for elements in openData
+         for key, value in elements.items() 
+         for keyJunior, valueJunior in value.items() 
+        ]
+        
+        dataDf = pd.DataFrame(flattenedData, columns = ['Ticker', 'Date', 'ROIC'])
+        dataDf['Date'] =  pd.to_datetime(dataDf['Date'], format='%Y-%m-%d')
+        dataDf['Date'] = dataDf.Date.dt.year
+        
+        groupedData = dataDf.groupby(['Date'])#.apply(lambda a: a[:])
+   
+        splitGrouped = [groupedData.get_group(x) for x in groupedData.groups][3:-1]
+
+        return splitGrouped
+        
+    
+    
+    '''
+    This function reurns the annual ROIC for each  
+    '''
+    def quintiledROIC(self):
+        pass
+    
+        
+    '''
+    This function calculates the cummulative annual growth rate (CAGR).
+    The Compound annual growth rates are arranged by quintile, 
+    based on the annually run portfolio returns.
+    The function takes quintile separated returns data and 
+    returns the CAGR for each quintile
+    '''
+    def cummulativeAnnualGrowthRateQuintiles(self):
+        pass
+        
+        
+        
+
+object  = ROIC(
+    '/Users/adamszequi/Desktop/Clones/ROIC /Data/Annual Historical Prices.json',
+    '/Users/adamszequi/Desktop/Clones/ROIC /Data/ROIC data.json'   
+)    
+
+print(object.splitDfYears())       
+
+for i in object.splitDfYears():
+    print(len(i))
